cb421b7b35b02b4e6a6a46b6978e30cb01ba764d
ijawadahmadcs <ijawadahmadcs@gmail.com>
2025-08-15 15:19:49 +0500

    portfolio

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-amber-200'>
+   
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/Components/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div className='bg-amber-50'>
+
+    </div>
+  )
+}
+
+export default Main
