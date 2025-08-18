886aca560bd4d7b38a899731ffd29a5714dca2ec
ijawadahmadcs <ijawadahmadcs@gmail.com>
2025-08-18 14:39:42 +0500

    projects-updated

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -21,8 +21,12 @@ export default function Projects() {
             <div className="p-6">
               <h3 className="text-xl font-bold mb-2">E-commerce Website</h3>
               <p className="text-gray-600 mb-4">
-                A fully responsive e-commerce platform with product listings,
-                shopping cart, and secure payment integration.
+                This project is a modern responsive e-commerce website built
+                with Next.js and Tailwind CSS, featuring product browsing,
+                category pages, cart management, and theme switching, product
+                listings, shopping cart, and secure payment integration.. It
+                provides a clean UI with authentication and a fully functional
+                shopping experience.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 <span className="bg-blue-100 px-2 py-1 rounded text-sm">
@@ -58,8 +62,11 @@ export default function Projects() {
             <div className="p-6">
               <h3 className="text-xl font-bold mb-2">Amazon Clone</h3>
               <p className="text-gray-600 mb-4">
-                Clone webpage of Amazon using HTML and modern Tailwind CSS.
-                Fully responsive design.
+                A Front-end clone of Amazon's homepage, created with plain HTML
+                and image assets. It mainly focuses on replicating the layout,
+                design, and structure of Amazon's interface as a practice
+                project, without any back-end or dynamic e-commerce
+                functionality
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 <span className="bg-green-100  px-2 py-1 rounded text-sm">
@@ -83,7 +90,7 @@ export default function Projects() {
           <div className="bg-white rounded-2xl shadow-xl hover:shadow-2xl ">
             <div className="relative">
               <img
-                src="images/prog2.png"
+                src="images/prog2.jpg"
                 alt="Travel And Tour Management System"
                 className="w-full h-48 object-cover hover:scale-110 transition-transform"
               />
@@ -94,8 +101,11 @@ export default function Projects() {
                 Travel And Tour Management System
               </h3>
               <p className="text-gray-600 mb-4">
-                A complete travel and tour management system with booking,
-                cancelling, and updating features.
+                Travel Management System with login authentication, offering
+                features like customer, package, and booking management. It
+                allows adding, viewing, searching, and deleting customer
+                records, package records, booking records, displaying details in
+                a structured table format.
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
                 <span className="bg-black text-white px-2 py-1 rounded text-sm">
@@ -104,6 +114,39 @@ export default function Projects() {
               </div>
               <a
                 href="https://github.com/ijawadahmadcs/travel-and-tour-management.git"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="flex items-center text-gray-600 hover:text-gray-800"
+              >
+                <FaGithub className="mr-1" /> Code
+              </a>
+            </div>
+          </div>
+          <div className="bg-white rounded-2xl shadow-xl hover:shadow-2xl ">
+            <div className="relative">
+              <img
+                src="images/prog4.jpg"
+                alt="Travel And Tour Management System"
+                className="w-full h-48 object-cover hover:scale-110 transition-transform"
+              />
+              <div className="absolute inset-0 bg-gradient-to-t from-black/50 to-transparent opacity-0 hover:opacity-100 transition-opacity" />
+            </div>
+            <div className="p-6">
+              <h3 className="text-xl font-bold mb-2">Snake Water Gun</h3>
+              <p className="text-gray-600 mb-4">
+                This Python project is a simple Snake-Water-Gun game where
+                players choose between snake (0), water (1), or gun (2) to
+                compete against the computer's random selection. The game tracks
+                rounds, declares winners based on the rules (e.g., gun beats
+                snake, water beats gun), and handles invalid inputs
+              </p>
+              <div className="flex flex-wrap gap-2 mb-4">
+                <span className="bg-gradient-to-r from-blue-400 to bg-yellow-300  px-2 py-1 rounded text-sm">
+                  Python
+                </span>
+              </div>
+              <a
+                href="https://github.com/ijawadahmadcs/snake-water-gun-game"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="flex items-center text-gray-600 hover:text-gray-800"
